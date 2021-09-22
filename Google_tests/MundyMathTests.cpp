9c28388c1f72cca7c9b0e3546ca7277cc2e1c5a5
Jason Schafer <jason59549@gmail.com>
2021-09-22 15:49:46 -0400

    Added Square Root Tests

--- Google_tests/MundyMathTests.cpp
+++ Google_tests/MundyMathTests.cpp
@@ -30,3 +30,14 @@ TEST(testMathSuite, divisionTest) {
 	EXPECT_EQ(3, MundyMath::divide(75, 25));
 	EXPECT_EQ(201, MundyMath::divide(100.5, 0.5));
 }
+
+TEST(testMathSuite, squareRootTest) {
+	EXPECT_EQ(7, MundyMath::squareRoot(49));
+	EXPECT_EQ(7, MundyMath::squareRoot(49.5));
+	EXPECT_EQ(7, MundyMath::divide(50));
+	EXPECT_EQ(3, MundyMath::divide(9));
+	EXPECT_EQ(574, MundyMath::squareRoot(329476));
+	EXPECT_EQ(10000, MundyMath::squareRoot(100000000));	
+}
+
+
