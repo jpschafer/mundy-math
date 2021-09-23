27f3800260196d4c7bb45fae366e1dace880e8c8
Jason Schafer <jason59549@gmail.com>
2021-09-23 12:21:56 -0400

    Scaffolding

--- Result.h
+++ Result.h
@@ -1,12 +1,50 @@
+#include <string>
+
+template <typename T>
+
 class Result {
 public:
 	T originalResult;
+	int base; // For string types specifically
+	
 	Result() {   cout<<"Constructor Called"<<endl;   }
 	
-	T getResult();
-	string toBinary();
+	T getResult() {
+		return originalResult;
+	};
+	
+	string toBinary() {
+		if (isStringBased) {
+		
+		} else {
+		
+		}
+	};
+	
 	string toOctal();
-	int toInteger();
-	double toDouble();
+	
+	int toInteger() {
+		if (isStringBased) {
+		
+		} else {
+		
+		}
+	};
+	
+	double toDouble() {
+		if (isStringBased) {
+		
+		} else {
+		
+		}
+	};
+	
+	bool isStringBased() {
+		if (isStringBased) {
+		
+		} else {
+		
+		}
+	}
   
 };
